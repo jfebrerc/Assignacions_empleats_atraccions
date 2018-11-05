3c40a37bdee351a1ebc53ebdbbdd0ebf15e2728e
jfebrerc <jfebrerc@gmail.com>
2018-11-05 17:48:28 +0100

    test

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,6 +1,8 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class main {
     private JButton registrarEmpleatButton;
@@ -11,7 +13,20 @@ public class main {
     private JButton sortirButton;
     private JPanel panelMain;
 
-    public static void main(String[] args){
+    public main() {
+        registrarEmpleatButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("main");
+        frame.setContentPane(new main().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
