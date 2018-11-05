5c4d9c9661c94f580cfad8da252aa7bf774fec42
jfebrerc <jfebrerc@gmail.com>
2018-11-05 17:35:09 +0100

    test

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,6 +1,16 @@
 package Main;
 
+import javax.swing.*;
+
 public class main {
+    private JButton registrarEmpleatButton;
+    private JButton eliminarEmpleatsButton;
+    private JButton modificarEmpleatsButton;
+    private JButton llistarEmpleatsButton;
+    private JButton testButton;
+    private JButton sortirButton;
+    private JPanel panelMain;
+
     public static void main(String[] args){
 
     }
